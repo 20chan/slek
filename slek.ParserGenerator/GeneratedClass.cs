f18f2937e26fe0d11ca76b5d90af86d380778b45
phillyai <sonagi.philly@gmail.com>
2018-08-27 09:07:06 +0900

    GeneratedClass 개선

--- slek.ParserGenerator/GeneratedClass.cs
+++ slek.ParserGenerator/GeneratedClass.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Text;
 
 namespace slek.ParserGenerator
 {
@@ -12,6 +13,8 @@ namespace slek.ParserGenerator
 
         public int BaseIndent { get; set; }
 
+        readonly string indent1, indent2, indent3, indent4, indent5;
+
         public GeneratedClass(int indent, Modifier m, string name, bool isinherited, string inhclass, params ClassField[] fields)
         {
             ClassModifier = m;
@@ -20,42 +23,76 @@ namespace slek.ParserGenerator
             InheritedClassName = inhclass;
             Fields = fields;
             BaseIndent = indent;
+
+            indent1 = new string(' ', BaseIndent);
+            indent2 = new string(' ', BaseIndent + 4);
+            indent3 = new string(' ', BaseIndent + 8);
+            indent4 = new string(' ', BaseIndent + 12);
+            indent5 = new string(' ', BaseIndent + 16);
         }
 
         public override string ToString()
         {
-            var builder = new System.Text.StringBuilder();
-            var indent = new string(' ', BaseIndent);
-            var indent2 = new string(' ', BaseIndent + 4);
-            var indent3 = new string(' ', BaseIndent + 8);
+            var builder = new StringBuilder();
 
-            builder.Append(indent);
+            builder.Append(indent1);
             builder.Append($"{ClassModifier} class {Name}");
             if (IsInherited)
                 builder.Append($" : {InheritedClassName}");
-            builder.Append(indent);
-            builder.AppendLine("{");
-            
-            foreach (var f in Fields)
-                builder.AppendLine($"{indent2}{f.Modifier} {f.Type} {f.Name};");
-
-            builder.Append($"public {Name}(");
-            
-            for (int i = 0; i < Fields.Length - 1; i++)
-                builder.Append($"{Fields[i].Type} {Fields[i].Name} ,");
-            builder.AppendLine($"{Fields.Last().Type} {Fields.Last().Name})");
-            builder.Append(indent2);
+            builder.AppendLine();
+            builder.Append(indent1);
             builder.AppendLine("{");
 
-            foreach (var f in Fields)
-                builder.AppendLine($"{indent3}this.{f.Name} = {f.Name}");
+            AppendFields(builder);
+            AppendConstructor(builder);
+            AppendEquals(builder);
 
-            builder.Append(indent2);
-            builder.AppendLine("}");
-
-            builder.Append(indent);
+            builder.Append(indent1);
             builder.AppendLine("}");
             return builder.ToString();
+        }
+
+        void AppendFields(StringBuilder sb)
+        {
+            foreach (var f in Fields)
+                sb.AppendLine($"{indent2}{f.Modifier} {f.Type} {f.Name};");
+        }
+
+        void AppendConstructor(StringBuilder sb)
+        {
+            sb.Append($"{indent2}public {Name}(");
+
+            for (int i = 0; i < Fields.Length - 1; i++)
+                sb.Append($"{Fields[i].Type} {Fields[i].Name}, ");
+            sb.AppendLine($"{Fields.Last().Type} {Fields.Last().Name})");
+            sb.Append(indent2);
+            sb.AppendLine("{");
+
+            foreach (var f in Fields)
+                sb.AppendLine($"{indent3}this.{f.Name} = {f.Name};");
+
+            sb.Append(indent2);
+            sb.AppendLine("}");
+        }
+
+        void AppendEquals(StringBuilder sb)
+        {
+            var returnfalse = $"{indent4}return false;";
+
+            sb.AppendLine($"{indent2}public override bool Equals(object obj)");
+            sb.AppendLine($"{indent2}{{");
+            sb.AppendLine($"{indent3}var node = obj as {Name};");
+            sb.AppendLine($"{indent3}if (node == null)");
+            sb.AppendLine(returnfalse);
+            sb.AppendLine($"{indent3}if (this.GetType() != node.GetType())");
+            sb.AppendLine(returnfalse);
+            foreach (var m in Fields)
+            {
+                sb.AppendLine($"{indent3}if (!(this.{m.Name}.Equals(node.{m.Name})))");
+                sb.AppendLine(returnfalse);
+            }
+            sb.AppendLine($"{indent3}return true;");
+            sb.AppendLine($"{indent2}}}");
         }
 
         public class ClassField
@@ -63,6 +100,13 @@ namespace slek.ParserGenerator
             public Modifier Modifier;
             public string Type;
             public string Name;
+
+            public ClassField(Modifier m, string type, string name)
+            {
+                Modifier = m;
+                Type = type;
+                Name = name;
+            }
         }
 
         public enum Modifier
